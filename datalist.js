15a93aab87dc9c7ff0663cf33c817fa5d191a0b7
ahlbomgunnar <Gahlbom@gmail.com>
2017-04-26 12:12:21 +0200

    rework

--- datalist.js
+++
@@ -1,7 +0,0 @@
-var linkList=[
-	{url:'https://www.google.se/', name:'Nyheter'},
-	{url:'https://www.google.se/', name:'Sport'},
-	{url:'https://www.google.se/', name:'Nöje'},
-	{url:'https://www.google.se/', name:'Väder'},
-	{url:'https://www.google.se/', name:'Diskussion'}
-]
