ea124d9564df2271c2f1689e604aa091f3333e08
Petri Jahnsson <meikyo72@gmail.com>
2017-05-08 18:05:43 +0200

    Sports section ready

--- sport.js
+++ sport.js
@@ -1,28 +1,55 @@
 
+function getTheData(sportData) {
+    fetch("https://newsapi.org/v1/articles?source=talksport&sortBy=top&apiKey=3c28b6d633ce4a0d86aa0e238453b4cf")
+            .then(function(response){return response.json()})
+                .then(function(json){
+                    let data = json.articles;
+                    sportData(data);
+                    
+    });
+}
     
-    var httpRequest;
-    var newsList;
-    httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = function() {
-        if (httpRequest.readyState == XMLHttpRequest.DONE) {
-            if (httpRequest.status == 200){
-                newsList = JSON.parse(this.responseText);
-                document.getElementById("headLine").innerHTML = newsList.articles[1].title;
-                document.getElementById("brodText").innerHTML = newsList.articles[1].description;
-                document.getElementById("author").innerHTML = `Author: ${newsList.articles[1].author} || Published at:${newsList.articles[1].publishedAt}`;
-                console.log(httpRequest.response);
-            }
-            else if (httpRequest.status == 400) {
-                console.log('There was an error 400');
-            }
-            else {
-                console.log(`Returned error was ${httpRequest.status}`);
-            }
-        }
+ 
+class App extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            sportNews: [] 
+        };
+        this.sportData = this.sportData.bind(this);
     };
-    
-    httpRequest.open('GET', 'https://newsapi.org/v1/articles?source=talksport&sortBy=top&apiKey=3c28b6d633ce4a0d86aa0e238453b4cf', true);
-    //httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    httpRequest.send();
-    
-    
+    componentDidMount() {
+        getTheData(this.sportData);
+    };
+    sportData(data) {
+        this.setState({
+            sportNews: data
+        });
+    }
+    render() {
+        let k = 0;
+        let newslist =  this.state.sportNews;
+        const newNews = newslist.map( x => <Artikel key={k++} title={x.title} description={x.description} urlToImage={x.urlToImage} author={x.author} publishedAt={x.publishedAt} url={x.url}/>);
+        return (
+            <div>
+                {newNews}
+            </div>
+        );
+    }
+}
+class Artikel extends React.Component {   
+    render () {
+            return (
+                <div>
+                <h1>{this.props.title}</h1>
+                <img src={this.props.urlToImage} width="100%"/>
+                <p>{this.props.description}</p>
+                <a href={this.props.url}>Read full article</a>
+                <p>By: {this.props.author} || {this.props.publishedAt}</p>
+                </div>
+    )
+}}
+ReactDOM.render(
+    <App></App>,
+    document.getElementById('content')
+)
